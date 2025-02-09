ab9c76d6ee6b53b29df9e3b62070fbc8aa437f79
Danilo Silva <contact@danilocgsilva.me>
2025-02-09 19:31:53 -0300

    Better code organization

--- www/src/Controller/FetchController.php
+++ www/src/Controller/FetchController.php
@@ -12,22 +12,13 @@ final class FetchController extends AbstractController
     #[Route('/fetch/{channelHash}', name: 'app_fetch')]
     public function index(string $channelHash, Fetch $fetch): JsonResponse
     {
-        $googleCloudApiKey = $this->getParameter('google_cloud_api_key');
-        $apiAddress = "https://www.googleapis.com/youtube/v3/search";
-        $apiAddressExample = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=CHANNEL_ID&key=YOUR_API_KEY";
-        $apiAddressExample2 = "https://www.googleapis.com/youtube/v3/channels?id={$channelHash}&key={$googleCloudApiKey}";
-        $apiAddressExample3 = "https://www.googleapis.com/youtube/v3/playlistItems?maxResults=50&playlistId=UPLOADS_PLAYLIST_ID&key={$googleCloudApiKey}";
-        $apiAddressExample4 = "https://www.googleapis.com/youtube/v3/channels?id={$channelHash}&key={$googleCloudApiKey}";
-        $apiAddressExample5 = "https://www.googleapis.com/youtube/v3/channels?id={$channelHash}&key={$googleCloudApiKey};";
-        $apiAddressExample6 = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id={$channelHash}&key={$googleCloudApiKey}";
-        $apiAddressExample7 = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id={$channelHash}&key={$googleCloudApiKey}";
-        $apiAddressExample8 = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=50&playlistId={$channelHash}&key={$googleCloudApiKey}&pageToken=";
+        /**
+         * @var array
+         */
+        $results = $fetch->fetch($channelHash);
 
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/FetchController.php',
-            'channelHash' => $channelHash,
-            'sampleAddress' => $apiAddressExample8,
+            'videos' => $results,
         ]);
     }
 }
--- www/src/Services/Fetch.php
+++ www/src/Services/Fetch.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\ClientInterface;
-use Symfony\Component\DependencyInjection\Attribute\AutowireCallable;
+use Exception;
 
 class Fetch
 {
@@ -17,19 +16,46 @@ class Fetch
 
     public function fetch(string $channelId)
     {
-        $url = sprintf(
+        $uploadsId = $this->getUploads($channelId);
+
+        $videos = [];
+        $rawContentsString = $this->fetchResults($uploadsId);
+
+        $contents = json_decode($rawContentsString);
+        foreach ($contents->items as $item) {
+            $videos[] = $item->snippet->title;
+        }
+
+        return $videos;
+    }
+
+    private function getUploads(string $channelId)
+    {
+        $urlIdList = sprintf(
             'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=%s&key=%s',
             $channelId,
             $this->apiKey
         );
 
-        $response = $this->httpClient->request("GET", $url);
-        $body = $response->getBody();
-        print_r(json_decode((string) $body));
+        $response = $this->httpClient->request("GET", $urlIdList);
+        $contents = json_decode($response->getBody()->getContents());
+
+        if (!empty($contents->items)) {
+            return $contents->items[0]->contentDetails->relatedPlaylists->uploads;
+        }
+        throw new Exception('Could not get uploads playlist ID');
     }
-
-    private function getUploads()
+ 
+    public function fetchResults(string $uploadsId): string
     {
-
+        $pagination = 50;
+        $urlToPaylist = sprintf(
+            'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=%s&playlistId=%s&key=%s',
+            $pagination,
+            $uploadsId,
+            $this->apiKey
+        );
+        $response = $this->httpClient->request("GET", $urlToPaylist);
+        return $response->getBody()->getContents();
     }
 }
